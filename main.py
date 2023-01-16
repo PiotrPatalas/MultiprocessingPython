9d80404081d739dd6d91eb14a9980fd5db16f610
Piotr Patalas <piotrpatalas67@gmail.com>
2023-01-16 16:26:34 +0100

    Zmiana struktury

--- main.py
+++ main.py
@@ -7,35 +7,59 @@ import os
 from PyPDF2 import PdfReader
 import re
 
-#odczyt i wyodrębnienie tekstu z pliku PDF za pomocą biblioteki PyPDF2 - POCZĄTEK
 
-pdffileobj = open('sample3.pdf', 'rb') # otwieramy plik w trybie binarnym 
-reader = PdfReader(pdffileobj) #inicjalizujemy obiekt reader 
+def main():
+    # odczyt PDF z PyPDF2
+    start_time1 = time.perf_counter()
+    # otwieramy plik w trybie binarnym
+    pdffileobj = open('Sample/sample4.pdf', 'rb')
+    finish_time1 = time.perf_counter()
+    reader = PdfReader(pdffileobj)  # inicjalizujemy obiekt reader
 
-iloscStron = len(reader.pages)
-print(iloscStron)
-
-page_content=""                
-for page_number in range(iloscStron):
-    page = reader.pages[page_number]
-    page_content += page.extract_text()
     
-#odczyt i wyodrębnienie tekstu z pliku PDF za pomocą biblioteki PyPDF2 - KONIEC
+    iloscStron = len(reader.pages)
+    print("Ilosc stron: ", iloscStron)
+    print("Ilosc procesorów", os.cpu_count())
 
-test_sub = "a"
-res = [i.start() for i in re.finditer(test_sub, page_content)]
+    page_content = ""
+    start_time2 = time.perf_counter()
+    for page_number in range(iloscStron):
+        page = reader.pages[page_number]
+        page_content += page.extract_text()
+    finish_time2 = time.perf_counter()
+    
+    
 
-print("String" + test_sub + "wystepuje na popzycjach" + str(res))
+    test_sub = "or"
+    start_time3 = time.perf_counter()
+    res = [i.start() for i in re.finditer(test_sub, page_content)]
+    finish_time3 = time.perf_counter()
+
+    outputMatchingIndexes(res)
+    print(f"Wczytanie pliku trwało: {finish_time1 - start_time1} sekund")
+    print(f"Konwersja na txt: {finish_time2 - start_time2} sekund")
+    print(f"Wyszukiwanie trwało: {finish_time3 - start_time3} sekund")
+    drawPlots(iloscStron)
+
+def pdfToText():
+    
 
 
-# filetxt=open(r"./pliktxt.txt","a")
-# filetxt.writelines(page_content)
+def drawPlots(iloscStron):
+    fig = plt.figure(figsize=(15, 10))
+    plt.bar(1, 15, color='b', width=0.3)
+    # plt.bar(no_processes, times, color='b', width = 0.3)
+    plt.xlabel("Liczba procesów")
+    plt.ylabel("Czas działania [s]")
+    plt.title("Konwersja PDF o dlugosci " + str(iloscStron) + " stron do txt następnie wyszukiwanie wzorca w tekście \nZależnie od ilosci procesorów [ 1 - " + str(os.cpu_count()) + "]")
+    # plt.savefig("wzorzec_w_tekscie_["+str(lb)+", " + str(ub)+"].png")
+    plt.show()
 
 
-# dlt=
-# plt.bar(no_processes, times, color='b', width = 0.3)
-# plt.xlabel("Liczba procesów")
-# plt.ylabel("Czas działania")
-# plt.title("Wyszukiwanie wzorca w tekście\nZakres wartosci ["+str(lb)+", " + str(ub)+"]")
-# plt.savefig("wzorzec_w_tekscie_["+str(lb)+", " + str(ub)+"].png")
-# plt.show()
+def outputMatchingIndexes(res):
+    filetxt = open(r"./Output/pliktxt.txt", "a")
+    filetxt.writelines(str(res))
+
+
+if __name__ == "__main__":
+    main()
